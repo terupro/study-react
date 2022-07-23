c294d8fc5288446826d53f504864228f374755e2
‘terupro’ <terupro911@gmail.com>
2022-07-23 23:34:17 +0900

    ✨ 講座用のuseEffectを導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,11 +3,20 @@ import Head from "next/head";
 import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
 import { Footer } from "src/components/Footer";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 
 export default function Home() {
   const handleClick = useCallback((e) => {
     e.preventDefault();
+  }, []);
+
+  useEffect(() => {
+    console.log("マウント");
+    document.body.style.backgroundColor = "lightblue";
+    return () => {
+      console.log("アンマウント");
+      document.body.style.backgroundColor = "";
+    };
   }, []);
   return (
     <div className={classes.container}>
