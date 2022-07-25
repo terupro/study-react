4f4d5846b00c51dfbd9716e2c3c6f693fee19f2c
‘terupro’ <terupro911@gmail.com>
2022-07-25 10:32:05 +0900

    ✨ useStateで配列を扱ってみた

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,6 +9,7 @@ export default function Home() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState("");
   const [isShow, setIsShow] = useState(true);
+  const [array, setArray] = useState([]);
 
   const handleClick = useCallback(() => {
     if (count < 10) {
@@ -19,6 +20,7 @@ export default function Home() {
   const handleDisplay = () => {
     setIsShow((prevIsShow) => !prevIsShow);
   };
+
   const handleChange = useCallback((e) => {
     if (e.target.value.length > 5) {
       alert("5文字以内にしてください");
@@ -27,9 +29,16 @@ export default function Home() {
     setText(e.target.value.trim());
   }, []);
 
-  const handleDisplay = () => {
-    setIsShow((isShow) => !isShow);
-  };
+  const handleAdd = useCallback(() => {
+    setArray((prevArray) => {
+      if (prevArray.some((item) => item === text)) {
+        alert("同じ要素が既に存在します。");
+        return prevArray;
+      }
+      const newArray = [...prevArray, text];
+      return newArray;
+    });
+  }, [text]);
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
@@ -48,6 +57,12 @@ export default function Home() {
       <button onClick={handleClick}>ボタン</button>
       <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
       <input type="text" value={text} onChange={handleChange} />
+      <button onClick={handleAdd}>追加</button>
+      <ul>
+        {array.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+      </ul>
       <Main page="index" />
       <Footer />
     </div>
