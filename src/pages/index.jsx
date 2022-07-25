3edcce1658ad2a44173535f45a34d6debba3bdaa
‘terupro’ <terupro911@gmail.com>
2022-07-25 10:31:12 +0900

    ♻️ 状態を変更するときにprevをつけてわかりやすくした

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,10 +12,13 @@ export default function Home() {
 
   const handleClick = useCallback(() => {
     if (count < 10) {
-      setCount((count) => count + 1);
+      setCount((prevCount) => prevCount + 1);
     }
   }, [count]);
 
+  const handleDisplay = () => {
+    setIsShow((prevIsShow) => !prevIsShow);
+  };
   const handleChange = useCallback((e) => {
     if (e.target.value.length > 5) {
       alert("5文字以内にしてください");
